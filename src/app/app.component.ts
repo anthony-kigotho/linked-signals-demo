e9a34ed2365ac6fa9570472058c51182d51fb228
anthony-kigotho <anthonykigotho7@gmail.com>
2025-01-08 12:01:13 +0300

    linkedSignal implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { signal, linkedSignal } from '@angular/core';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'linked-list-demo';
+  title = 'linked-signal-demo';
+  shippingOptions = signal(['Ground', 'Air', 'Sea']); // Available shipping methods.
+  selectedOption = linkedSignal(() => this.shippingOptions()[0]); // Automatically defaults to the first option.
+
+  changeShipping(newOptionIndex: number) {
+    this.selectedOption.set(this.shippingOptions()[newOptionIndex]); // User can change the selection.
+  }
+
+  testLinkedSignal() {
+    this.shippingOptions.set(['Email', 'Will Call']);
+    console.log(this.selectedOption()); // Automatically updates to 'Email'.
+  }
+
+  ngOnInit() {
+    this.testLinkedSignal();
+  } 
 }
+
+
